32d12f79bb2007cecbf460b688a1fe5b4e3898ed
Dave Glover <dglover@microsoft.com>
2017-12-01 23:38:23 +1100

    simplified mechanism for retry of ntp time

--- lib/iothub.py
+++ lib/iothub.py
@@ -3,6 +3,7 @@ import base64
 import hmac
 import urlencode
 import ntptime
+import time
 
 
 class IotHub():
@@ -13,32 +14,32 @@ class IotHub():
         self.hubUser = hubAddress + '/' + deviceId
         self.hubTopicPublish = 'devices/' + deviceId + '/messages/events/'
         self.hubTopicSubscribe = 'devices/' + deviceId + '/messages/devicebound/#'
-        
+
     # sas generator from https://github.com/bechynsky/AzureIoTDeviceClientPY/blob/master/DeviceClient.py
-    def generate_sas_token(self, expiry=3600): # default to one hour expiry period
+    def generate_sas_token(self, expiry=3600):  # default to one hour expiry period
         urlencoder = urlencode.Urlencode()
 
-        # Time Epoch: Unix port uses standard for POSIX systems epoch of 1970-01-01 00:00:00 UTC. 
+        # Time Epoch: Unix port uses standard for POSIX systems epoch of 1970-01-01 00:00:00 UTC.
         # However, embedded ports use epoch of 2000-01-01 00:00:00 UTC.
         # 946684800 is the offset from embedded epoch and unix epoch bases
-        retry = True
+
         now = 0
 
-        while retry:
-          try:
-            now = ntptime.time() + 946684800
-            retry = False
-          except:
-            retry = True
+        while now == 0:
+            try:
+                now = ntptime.time() + 946684800
+            except:
+                time.sleep(1)
 
-        ttl = now + expiry        
-        urlToSign = urlencoder.quote(self.endpoint)         
+        ttl = now + expiry
+        urlToSign = urlencoder.quote(self.endpoint)
         sign_sharedAccessKey = "%s\n%d" % (urlToSign, int(ttl))
         msg = "{0}\n{1}".format(urlToSign, ttl).encode('utf-8')
         key = base64.b64decode(self.sharedAccessKey)
-        h = hmac.HMAC(key, msg = msg, digestmod=sha256)
+        h = hmac.HMAC(key, msg=msg, digestmod=sha256)
         decodedDigest = base64.b64encode(h.digest()).decode()
         signature = urlencoder.quote(decodedDigest)
-        sas = "SharedAccessSignature sr={0}&sig={1}&se={2}".format(urlToSign, signature, ttl)
+        sas = "SharedAccessSignature sr={0}&sig={1}&se={2}".format(
+            urlToSign, signature, ttl)
         # print(sas)
         return sas
