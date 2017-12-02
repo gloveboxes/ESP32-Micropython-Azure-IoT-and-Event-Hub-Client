aebd52c07adc4632c7bc7fb562ee25f8fa5f0e2d
Dave Glover <dglover@microsoft.com>
2017-12-02 21:23:19 +1100

    added endpoint property to iothub

--- lib/iothub.py
+++ lib/iothub.py
@@ -16,29 +16,29 @@ class IotHub():
 
 
     # sas generator from https://github.com/bechynsky/AzureIoTDeviceClientPY/blob/master/DeviceClient.py
-    def generate_sas_token(self, expiry=3600):  # default to one hour expiry period
-        urlencoder = urlencode.Urlencode()
+    def generate_sas_token(self, expiry = 3600):  # default to one hour expiry period
+        urlencoder=urlencode.Urlencode()
 
         # Time Epoch: Unix port uses standard for POSIX systems epoch of 1970-01-01 00:00:00 UTC.
         # However, embedded ports use epoch of 2000-01-01 00:00:00 UTC.
         # 946684800 is the offset from embedded epoch and unix epoch bases
 
-        now = 0
+        now=0
 
         while now == 0:
             try:
-                now = ntptime.time() + 946684800
+                now=ntptime.time() + 946684800
             except:
                 time.sleep(1)
 
-        ttl = now + expiry
-        urlToSign = urlencoder.quote(self.sasUrl)
-        msg = "{0}\n{1}".format(urlToSign, ttl).encode('utf-8')
-        key = base64.b64decode(self.sharedAccessKey)
-        h = hmac.HMAC(key, msg=msg, digestmod=sha256)
-        decodedDigest = base64.b64encode(h.digest()).decode()
-        signature = urlencoder.quote(decodedDigest)
-        sas = "SharedAccessSignature sr={0}&sig={1}&se={2}".format(
+        ttl=now + expiry
+        urlToSign=urlencoder.quote(self.sasUrl)
+        msg="{0}\n{1}".format(urlToSign, ttl).encode('utf-8')
+        key=base64.b64decode(self.sharedAccessKey)
+        h=hmac.HMAC(key, msg = msg, digestmod = sha256)
+        decodedDigest=base64.b64encode(h.digest()).decode()
+        signature=urlencoder.quote(decodedDigest)
+        sas="SharedAccessSignature sr={0}&sig={1}&se={2}".format(
             urlToSign, signature, ttl)
         # print(sas)
         return sas
